e3a2fa7f0fc072b627d0361f86d0191d8be5ecd1
withseon <dlstjsvv@gmail.com>
2023-11-23 22:53:11 +0900

    CoreData를 이용한 간단한 ToDoList

---
+++ LectureExample/CoreDataToDoList/CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample3
+//
+//  Created by 정인선 on 11/23/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ LectureExample/CoreDataToDoList/CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  CoreDataExample3
+//
+//  Created by 정인선 on 11/23/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
